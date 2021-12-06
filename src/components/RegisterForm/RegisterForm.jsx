facc323e0fe098c84379f33f0d8a55ea77a9c6f5
matheusafonsouza <matheusafonsouza@gmail.com>
2021-12-06 11:29:15 -0300

    feat: using materialui textfields and button

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import { TextField, Button } from '@material-ui/core';
 
 const RegisterForm = () => {
   return (
     <form>
-      <input type="text" />
+      <TextField
+        id='outlined-basic'
+        label='Nome'
+        variant='outlined'
+        type='text'
+      />
+      <TextField
+        id='outlined-basic'
+        label='Sobrenome'
+        variant='outlined'
+        type='text'
+      />
+      <TextField
+        id='outlined-basic'
+        label='CPF'
+        variant='outlined'
+        type='text'
+      />
+
+      <label>Promoções</label>
+      <input type='checkbox' />
+
+      <label>Novidades</label>
+      <input type='checkbox' />
+
+      <Button
+        type='submit'
+        variant='contained'
+        color='primary'
+      >
+        Cadastrar
+      </Button>
     </form>
   );
 };
