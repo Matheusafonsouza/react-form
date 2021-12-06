25a20f86b77fb55316457e14380e151f56b4c627
matheusafonsouza <matheusafonsouza@gmail.com>
2021-12-06 12:37:34 -0300

    feat: create function to use step forms

--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import { Typography } from '@material-ui/core';
+import React, { useState } from 'react';
 import ClientForm from './ClientForm';
 import DeliveryForm from './DeliveryForm';
 import UserForm from './Userform';
 
 const RegisterForm = ({ onSubmit }) => {
+  const [actualStep, setActualStep] = useState(0);
+
+  const renderStepForm = (step) => {
+    switch (step) {
+      case 0:
+        return <ClientForm />
+      case 1:
+        return <UserForm />
+      case 2:
+        return <DeliveryForm />
+      default:
+        return <Typography>Erro ao selecionar formulário</Typography>
+    }
+  };
+
   return (
     <>
-      <ClientForm />
-      <UserForm />
-      <DeliveryForm />
+      {renderStepForm(actualStep)}
     </>
   );
 };
